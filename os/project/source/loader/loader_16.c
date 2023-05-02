8db947baa7102a2c883b027f83c5ba9a4197a230
binarywz <binarywz@163.com>
2023-05-02 11:02:58 +0800

    使用内联汇编显示字符串

--- os/project/source/loader/loader_16.c
+++ os/project/source/loader/loader_16.c
@@ -6,8 +6,20 @@
 // 16位代码，必须加上放在开头，以便有些io指令生成为16位
 __asm__(".code16gcc");
 
-void loader_entry(void) {
-    for(;;) {}
+static void show_msg(const char* msg) {
+    char c;
+
+	// 使用bios写显存，持续往下写
+	while ((c = *msg++) != '\0') {
+		__asm__ __volatile__(
+            "mov $0xe, %%ah;"
+            "mov %[ch], %%al;"
+            "int $0x10"::[ch]"r"(c) // [ch]为操作数指定一个符号名
+        );
+	}
 }
 
-
+void loader_entry(void) {
+    show_msg("....loading.....\r\n");
+    for(;;) {}
+}
