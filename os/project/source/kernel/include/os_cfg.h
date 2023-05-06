1f6db934c718e9a612e17a2fcf9fc8d41c3785a9
binarywz <binarywz@163.com>
2023-05-06 23:56:27 +0800

    创建GDT表及其表项

---
+++ os/project/source/kernel/include/os_cfg.h
@@ -0,0 +1,9 @@
+/**
+ * os配置
+ */
+#ifndef OS_OS_CFG_H
+#define OS_OS_CFG_H
+
+#define GDT_TABLE_SIZE  256 // GDT表项数量
+
+#endif //OS_OS_CFG_H
--- os/project/source/kernel/init/init.c
+++ os/project/source/kernel/init/init.c
@@ -2,12 +2,13 @@
  * 内核初始化以及测试代码
  */
 #include "common/boot_info.h"
-
+#include "cpu/cpu.h"
 
 /**
  * 内核入口
  * 汇编调用C函数如何传参: 通过栈传递参数
  */
 void kernel_init(boot_info_t* boot_info) {
-    for (;;) {}
+    cpu_init();
+    for(;;) {}
 }
