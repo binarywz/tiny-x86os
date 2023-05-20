6ef8669acfce97b40b70b4dee0d635d2cdf497b2
binarywz <binarywz@163.com>
2023-05-20 22:35:29 +0800

    利用宏重用异常处理代码

--- os/project/source/kernel/cpu/irq.c
+++ os/project/source/kernel/cpu/irq.c
@@ -18,10 +18,31 @@ void do_handler_unknown(exception_frame_t* frame) {
 	do_default_handler(frame, "Unknown exception.");
 }
 
+void do_handler_divider(exception_frame_t* frame) {
+	do_default_handler(frame, "Divide Error.");
+}
+
 void irq_init(void) {	
 	for (uint32_t i = 0; i < IDT_TABLE_NR; i++) {
         gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t) exception_handler_unknown,
                   GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
 	}
+
+	// 设置异常处理接口
+	irq_install(IRQ0_DE, (irq_handler_t)exception_handler_divider);
+
 	lidt((uint32_t)idt_table, sizeof(idt_table));
+}
+
+/**
+ * @brief 安装中断或异常处理程序
+ */
+int irq_install(int irq_num, irq_handler_t handler) {
+	if (irq_num >= IDT_TABLE_NR) {
+		return -1;
+	}
+
+    gate_desc_set(idt_table + irq_num, KERNEL_SELECTOR_CS, (uint32_t) handler,
+                  GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
+	return 0;
 }
