fafc1309266bec2fe26b6ff6d1116d25ea814c72
binarywz <binarywz@163.com>
2023-05-23 23:49:20 +0800

    初始化中断控制器

--- os/project/source/kernel/include/cpu/irq.h
+++ os/project/source/kernel/include/cpu/irq.h
@@ -25,6 +25,27 @@
 #define IRQ19_XM            19
 #define IRQ20_VE            20
 
+// PIC控制器相关的寄存器及位配置
+#define PIC0_ICW1			0x20
+#define PIC0_ICW2			0x21
+#define PIC0_ICW3			0x21
+#define PIC0_ICW4			0x21
+#define PIC0_OCW2			0x20
+#define PIC0_IMR			0x21
+
+#define PIC1_ICW1			0xa0
+#define PIC1_ICW2			0xa1
+#define PIC1_ICW3			0xa1
+#define PIC1_ICW4			0xa1
+#define PIC1_OCW2			0xa0
+#define PIC1_IMR			0xa1
+
+#define PIC_ICW1_ICW4		(1 << 0)		// 1 - 需要初始化ICW4
+#define PIC_ICW1_ALWAYS_1	(1 << 4)		// 总为1的位
+#define PIC_ICW4_8086	    (1 << 0)        // 8086工作模式
+
+#define IRQ_PIC_START		0x20			// PIC中断起始号
+
 /**
  * 中断发生时相应的栈结构，暂时为无特权级发生的情况
  */
